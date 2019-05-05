8a1f902753442f18a1b991bcc4f07d4c01e63905
Vivvianne Kimani <ndiahav@gmail.com>
2019-05-05 23:31:47 +0300

    attempted to create hover effect on portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -24,26 +24,32 @@
 
 
 
-
+//function to toggle between the icons and the text
 $(document).ready(function(){
     
-    $(".btn_1").click(function(){
-        $("p.par_1").toggle();   
+    $(".btn_1").click(function(){ 
+        $("p.par_1").show();
+        $(".btn_1").hide(); 
     });
     $("p.par_1").click(function(){
-        $(".btn_1").toggle();
+        $("p.par_1").hide();
+        $(".btn_1").show(); 
     });
      $(".btn_2").click(function(){
-         $("p.par_2").toggle();   
+         $("p.par_2").show();   
+         $(".btn_2").hide();
      });
      $("p.par_2").click(function(){
-         $(".btn_2").toggle();
+         $("p.par_2").hide();
+         $(".btn_2").show();
      });
      $(".btn_3").click(function(){
-         $("p.par_3").toggle();   
+         $("p.par_3").show();   
+         $(".btn_3").hide();
      });
      $("p.par_3").click(function(){
-         $(".btn_3").toggle();
+         $(".btn_3").show();
+         $("p.par_3").hide();
      }); 
   });
 
