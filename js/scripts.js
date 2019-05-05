48fc3987ba0fd1e85c5e0948f4bd81ea615ba80f
Vivvianne Kimani <ndiahav@gmail.com>
2019-05-05 13:29:05 +0300

    added the .click to make the text in what-we-do section hide and show

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-    $(".clickable").click(function(){
-        $(".pdesign-showing").toggle();
-        $(".pdesign-hidden").toggle();
-    })
-})
+    
+    $(".btn_1").click(function(){
+        $("p.par_1").toggle();   
+    });
+    $("p.par_1").click(function(){
+        $(".btn_1").toggle();
+    });
+     $(".btn_2").click(function(){
+         $("p.par_2").toggle();   
+     });
+     $("p.par_2").click(function(){
+         $(".btn_2").toggle();
+     });
+     $(".btn_3").click(function(){
+         $("p.par_3").toggle();   
+     });
+     $("p.par_3").click(function(){
+         $(".btn_3").toggle();
+     });
+  });
+    
